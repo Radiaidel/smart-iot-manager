159255226d682e298e4ea65c57e8ac07a706f615
radia idelkadi <idelkadiradia@gmail.com>
2024-12-19 10:25:32 +0100

    first commit

---
+++ src/main/java/com/era/smart_iot_manager/SmartIotManagerApplication.java
@@ -0,0 +1,13 @@
+package com.era.smart_iot_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartIotManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartIotManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/era/smart_iot_manager/SmartIotManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.era.smart_iot_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartIotManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
